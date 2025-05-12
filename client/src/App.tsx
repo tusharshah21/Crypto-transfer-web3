8e38ca6283e45c3bed35c7a4ad26099e76111fc4
tushar kumar shah <tusharkumarshah14394@gmail.com>
2025-05-12 16:40:09 +0530

    setup formation

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+const App = ()=>{
+  return (
+    <div className="App">
+      <div className="App-div">
+        <h1 className="text-l">Web3 Transfer</h1>
+        <p>
+          This is a simple web3 transfer application. You can send and receive
+          tokens using this application.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
