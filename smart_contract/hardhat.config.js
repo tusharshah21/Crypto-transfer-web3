c9a8b339af944e7e3a0b1d2402c3786684f50e0d
tushar kumar shah <tusharkumarshah14394@gmail.com>
2025-05-16 16:41:03 +0530

    added pvtkey package

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
-      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
+    sepolia: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
