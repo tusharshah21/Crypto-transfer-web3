0c83e72e5faa8c6d6a98506d4157fd4009d21f5c
tusharshah21 <tusahrkumarshah14394@gmail.com>
2025-06-10 13:29:18 +0530

    updated utils according to backend

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0x1061d902c140E15e04bf4d770cD587e880543deB";
+export const contractAddress = "0x4BB3AaE29639d13b4dE1cC2E34e7bD34Ee1d5603";
 export const contractABI = abi.abi;
